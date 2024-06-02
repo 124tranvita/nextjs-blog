5ff8e7da030e83a0c12d45f6f9c1a39ef69d6ebf
124tranvita <124tranvita@gmail.com>
2024-06-02 14:48:37 +0000

    fix build error: internal api

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,9 @@ import { LIMIT, PAGE_INIT } from "@/app/common/lib/constants";
 import { Loader } from "./loader";
 import Home from "@/app/common/ui/home";
 
+// https://github.com/vercel/next.js/issues/44062
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
   const initialPosts = await getPosts(PAGE_INIT, LIMIT);
 
